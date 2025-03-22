de4af4d770410ae54840e8bd10bde0f604525682
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2025-03-22 07:42:20 +0200

    update

--- orders/AddOrderDetails.php
+++ orders/AddOrderDetails.php
@@ -2,7 +2,6 @@
 
 include("../connection.php");
 
-$o_id = filterReq("o_id");
 $quantity = filterReq("quantity");
 $name = filterReq("name");
 $img = filterReq("img");
@@ -10,36 +9,47 @@ $desc = filterReq("desc");
 $price = filterReq("price");
 
 $stmt = $conn->prepare(
-    "INSERT INTO `order_details` (`o_id` , `quantity` , `name` , `img` , `desc` , `price`)
-        VALUES ( ?,?,? ,?,?,?) "
+    "SELECT `id` FROM `orders` ORDER BY `id` DESC LIMIT 1"
 );
-$stmt->execute(
-    [
-        $o_id,
-        $quantity,
-        $name,
-        $img,
-        $desc,
-        $price,
-    ]
-);
+$stmt->execute();
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-$count = $stmt->rowCount();
+if ($result) {
 
-if ($count > 0) {
-    http_response_code(200);
-    echo json_encode(
+    $o_id = $result["id"];
+
+    $stmt = $conn->prepare(
+        "INSERT INTO `order_details` (`o_id` , `quantity` , `name` , `img` , `desc` , `price`)
+            VALUES ( ?,?,? ,?,?,?) "
+    );
+    $stmt->execute(
         [
-            "status" => 200,
-            "message" => "Order details added successfully"
+            $o_id,
+            $quantity,
+            $name,
+            $img,
+            $desc,
+            $price,
         ]
     );
-} else {
-    http_response_code(400);
-    echo json_encode(
-        [
-            "status" => 400,
-            "message" => "Order details not added"
-        ]
-    );
+
+    $count = $stmt->rowCount();
+
+    if ($count > 0) {
+        http_response_code(200);
+        echo json_encode(
+            [
+                "status" => 200,
+                "message" => "Order details added successfully"
+            ]
+        );
+    } else {
+        http_response_code(400);
+        echo json_encode(
+            [
+                "status" => 400,
+                "message" => "Order details not added"
+            ]
+        );
+    }
 }
