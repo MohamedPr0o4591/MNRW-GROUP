40e3e0d0ea9c97ef81d8938ffe3c490b291b8f54
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2025-04-10 06:05:02 +0200

    update

--- visa card/upload_visa.php
+++ visa card/upload_visa.php
@@ -1,13 +1,13 @@
 <?php
 include("../connection.php");
 
-$c_number = patchData("c_number");
-$ex_month = patchData("ex_month");
-$ex_year = patchData("ex_year");
-$cvv = patchData("cvv");
-$c_holder = patchData("c_holder");
+$c_number = filterReq("c_number");
+$ex_month = filterReq("ex_month");
+$ex_year = filterReq("ex_year");
+$cvv = filterReq("cvv");
+$c_holder = filterReq("c_holder");
 $created_at = date("Y-m-d H:i:s");
-$u_id = patchData("u_id");
+$u_id = filterReq("u_id");
 
 $stmt = $conn->prepare(
     "UPDATE `visa_cards` SET `c_number` =?, `ex_month` =?, `ex_year` =?, `cvv` =?, `c_holder` =?, `created_at` =? 
