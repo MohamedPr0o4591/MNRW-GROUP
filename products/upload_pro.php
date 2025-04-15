798d6845681b9ad22a391ad7b99f71534daeae7e
MohamedPr0o4591 <ev9ry.24hours@gmail.com>
2025-04-15 20:32:31 +0200

    update

--- products/upload_pro.php
+++ products/upload_pro.php
@@ -8,9 +8,21 @@ $desc = filterReq("desc");
 $img = uploadFile("img");
 $company_id = filterReq("company_id");
 
+$ram = filterReq("ram");
+$hard = filterReq(("hard"));
+$processor = filterReq("processor");
+$screen = filterReq("screen");
+$camera = filterReq("camera");
+$battery = filterReq("battery");
+$system_info = filterReq("system_info");
+$good_de = filterReq(("good_de"));
+$bad_de = filterReq(("bad_de"));
+
+
 $stmt = $conn->prepare(
     "INSERT INTO `products` (`name` , `price` , `img` ,`desc` , `company_id`) 
-        VALUES (?,?,?,?,?)"
+        VALUES (?,?,?,?,?) ;
+   "
 );
 $stmt->execute(
     [
@@ -19,24 +31,51 @@ $stmt->execute(
         $img,
         $desc,
         $company_id
+
     ]
 );
+
 $count = $stmt->rowCount();
 
 if ($count > 0) {
-    http_response_code(200);
-    echo json_encode(
+    $last_id = $conn->lastInsertId();
+    $stmt = $conn->prepare(
+        " INSERT INTO `info_product` (`ram`, `hard`, `procss`, `display`, `camireaa`, `battry`, `system_info`
+    ,`good_de`, `bad_de` , `product_id`)
+        VALUES (?,?,?,?,?,?,?,?,?,?)"
+    );
+    $stmt->execute(
         [
-            "status" => 200,
-            "message" => "Product uploaded successfully"
+            $ram,
+            $hard,
+            $processor,
+            $screen,
+            $camera,
+            $battery,
+            $system_info,
+            $good_de,
+            $bad_de,
+            $last_id
         ]
     );
-} else {
-    http_response_code(400);
-    echo json_encode(
-        [
-            "status" => 400,
-            "message" => "Product upload failed"
-        ]
-    );
+
+    $count = $stmt->rowCount();
+
+    if ($count > 0) {
+        http_response_code(200);
+        echo json_encode(
+            [
+                "status" => 200,
+                "message" => "Product uploaded successfully"
+            ]
+        );
+    } else {
+        http_response_code(400);
+        echo json_encode(
+            [
+                "status" => 400,
+                "message" => "Product upload failed"
+            ]
+        );
+    }
 }
