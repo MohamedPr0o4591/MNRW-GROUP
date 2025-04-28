d643fefdef7905e8b235dded7f9619a14b77d262
mohamedpr0o4591 <ev9ry.24hours@gmail.com>
2025-04-28 19:30:08 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true, // ⭐ إضافة مهمة جداً
+  },
+  preview: {
+    historyApiFallback: true, // ⭐ كمان هنا لو بتجرب ب vite preview
+  }
 })
